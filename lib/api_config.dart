394db76ae2402c9b6af72e8054238b9516585497
Manohar <manohar.a@calibrage.in>
2023-09-15 11:49:45 +0530

    completed of login api and scan qr also UI as well

---
+++ lib/api_config.dart
@@ -0,0 +1,7 @@
+library APIConstants;
+
+const String SUCCESS_MESSAGE=" You will be contacted by us very soon.";
+
+var baseUrl = "http://182.18.157.215/3FAkshaya/API/api/";
+
+var getfarmerlogin ="farmer/SendOTP/";  //in the place of farmercode farmercode should send
